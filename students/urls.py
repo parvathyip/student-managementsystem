2fe7fee70fe2208858f847ea88ea44cf7e46ec05
parvathy <parvathyprasad505@gmail.com>
2022-09-06 14:10:12 +0530

    first commit

---
+++ students/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:id>', views.view_student, name='view_student'),
+    # <int:id>id of student, which helps to create dynamic urls which matches the int
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete'),
+]
